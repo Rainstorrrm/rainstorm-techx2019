44c62abc0a759764c1ff56b5e30d97e13a2232ca
CharlieRuan0130 <53290280+CharlieRuan0130@users.noreply.github.com>
2019-07-28 14:22:12 +0800

    Update celebrate.py

--- celebrate.py
+++ celebrate.py
@@ -4,18 +4,19 @@ import rospy, time
 from geometry_msgs.msg import Twist
 length = 0.5
 
-#当投进了，或者已经跑到主人这里了
+#当投进了，或者已经跑到主人这里了, 
 def celebrate():
-  pub = rospy.Publisher('car/cmd_vel', Twist, queue_size=10)
-  rospy.init_node('publisher')
-  while not rospy.is_shutdown():
-      msg = Twist()
-    	msg.angular.z = 66
-      pub.publish(msg)
-	    time.sleep(length)
-      msg.angular.z = 114
-      pub.publish(msg)
-      time.sleep(length)
+    pub = rospy.Publisher('car/cmd_vel', Twist, queue_size=10)
+    rospy.init_node('publisher')
+    
+    while not rospy.is_shutdown():
+        msg = Twist()
+        msg.angular.z = 66
+        pub.publish(msg)
+        time.sleep(length)
+        msg.angular.z = 114
+        pub.publish(msg)
+        time.sleep(length)
       
 if __name__ == '__main__':
     try:
