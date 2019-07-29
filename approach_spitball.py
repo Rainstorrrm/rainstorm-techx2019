8fd13d831ac82a88489f75d1aab9d6f78b0628c5
CharlieRuan0130 <53290280+CharlieRuan0130@users.noreply.github.com>
2019-07-29 09:41:29 +0800

    Update approach_spitball.py

--- approach_spitball.py
+++ approach_spitball.py
@@ -1,2 +1,13 @@
 def approach_spitball.py();
+    #use cv to approach spitball
+    dist_threshold = 5
+    close_to_spitball = False
+    while not close_to_spitball:
+        if distance_to_spitball() < dist_threshold:
+            close_to_spitball = True
+        else:
+            pass
     
+    
+            
+        
